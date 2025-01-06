d3a21bf1a09722d29bb1acdd50fc73586068000b
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2025-01-06 21:49:15 +0000

    Added debounce to screensaver

--- app/static/js/display.js
+++ app/static/js/display.js
@@ -13,9 +13,11 @@ drawer_btn.onclick = () => {
 
 // Screensaver functionality
 let inactivityTimer;
+let debounceTimer;
 
 function resetInactivityTimer() {
   clearTimeout(inactivityTimer);
+  clearTimeout(debounceTimer);
   // Only set timer if screensaver delay is greater than 0
   if (screensaver_delay > 0) {
     inactivityTimer = setTimeout(() => {
@@ -24,13 +26,18 @@ function resetInactivityTimer() {
   }
 }
 
+function debounceResetInactivityTimer() {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(resetInactivityTimer, 200);  // 200ms debounce
+}
+
 // Only set up event listeners if screensaver is enabled
 if (screensaver_delay > 0) {
   // Reset timer on any interaction
   document.addEventListener('mousemove', resetInactivityTimer);
   document.addEventListener('keypress', resetInactivityTimer);
   document.addEventListener('click', resetInactivityTimer);
-  document.addEventListener('touchstart', resetInactivityTimer);
+  document.addEventListener('touchstart', debounceResetInactivityTimer);
 
   // Initial setup of timer
   resetInactivityTimer();
