b7a2c620a9054eae6b58c806466dc1fa770529b5
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2025-01-17 22:14:02 -0700

    Refactor to allow for PyCharm Endpoints functionality
    Signed-off-by: iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,18 @@
 from flask import Flask
 
+app = Flask(__name__)
 
-def create_app():
-	app = Flask(__name__)
+# Initialize Flask extensions here
 
-	# Initialize Flask extensions here
+# Register blueprints here
+from app.main import bp as main_bp
 
-	# Register blueprints here
-	from app.main import bp as main_bp
-	app.register_blueprint(main_bp)
+app.register_blueprint(main_bp)
 
-	from app.weather import bp as weather_bp
-	app.register_blueprint(weather_bp)
+from app.weather import bp as weather_bp
 
-	from app.photos import bp as photos_bp
-	app.register_blueprint(photos_bp)
+app.register_blueprint(weather_bp)
 
-	return app
+from app.photos import bp as photos_bp
+
+app.register_blueprint(photos_bp)
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
-from app import create_app
+from app import app
 
 if __name__ == '__main__':
-    app = create_app()
     app.run("localhost", 8000)
 else:
-    gunicorn_app = create_app()
-
+    gunicorn_app = app
